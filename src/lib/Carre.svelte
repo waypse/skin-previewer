de65beb55e89712e1d2c6e541e329c77736faf4c
waypse <slavapankov20@gmail.com>
2023-10-10 00:31:11 +0200

    first commit

---
+++ src/lib/Carre.svelte
@@ -0,0 +1,29 @@
+<script>
+    const nigga = "nigga";
+    const color = "#a4a8a796"
+    
+</script>
+
+
+<div>
+    <div class="inte"></div>
+</div>
+
+
+<style>
+    div {
+        width: 80vw;
+        height: 80vh;
+        background-color: #8be0cb96;
+        margin-bottom: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .inte {
+        width: 90%;
+        height: 80%;
+        background-color: white;
+        border-radius: 10px;
+    }
+</style>
